4360d169822489000762aa4aa0d9a2bc6aa4c85a
Thiboss <thibault.charbonnier@edu.devinci.fr>
2024-04-01 12:03:36 +0200

    Modif server

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import json
+from collections import Counter
+from scrapping import scrape
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+#Root pour exécuter le script de scrapping
+@app.route('/api/get_data')
+def get_data():
+   scrape()
+   return "Scrapping done"
 
 if __name__ == '__main__':
     app.run()
