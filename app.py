213aca33d67f4b3e80557c4fa1df661855c82c20
MathisRaimbault7 <math.raimb7@gmail.com>
2024-04-02 21:21:43 +0200

    Mis à jour. API fonctionnel v1. On peut chercher la dernière publication et celle en fonction d'un thème.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from flask import Flask, render_template
 import requests
-from flask import Flask, jsonify, render_template
-import feedparser  # We'll use feedparser to parse the XML response
+import feedparser
 
 app = Flask(__name__)
 
@@ -10,28 +10,65 @@ ARXIV_API_URL = "http://export.arxiv.org/api/query"
 
 @app.route('/')
 def home():
-    return 'Welcome to our API!'
+    return render_template('home.html')
 
-@app.route('/page')
-def latest_publications():
+# La dernière publication parmi toutes les publications
+@app.route('/search/last')
+def latest_publication():
     params = {
-        "search_query": "all:Autoregressive",
+        "search_query": "e",
         "sortBy": "submittedDate",
         "sortOrder": "descending",
-        "max_results": 1
+        "max_results": 5 
     }
     response = requests.get(ARXIV_API_URL, params=params)
     feed = feedparser.parse(response.content)
-    entries = [
-            {
-                'title': entry.title,
-                'authors': [author.name for author in entry.authors],
-                'summary': entry.summary,
-                'published': entry.published
-            }
-            for entry in feed.entries
-        ]
+
+    if not feed.entries:
+        return render_template('page.html', entries=None, message="Aucune publication récente trouvée.")
+
+    # Créer une liste de toutes les publications à partir de feed.entries
+    publications = []
+    for entry in feed.entries:
+        authors = [author.name for author in entry.authors] if entry.authors else 'Anonymous'
+        publication = {
+            'title': entry.title,
+            'authors': authors,
+            'summary': entry.summary,
+            'published': entry.published
+        }
+        publications.append(publication)
+
+    # Passer la liste des publications au template
+    return render_template('page.html', entries=publications)
+
+
+
+# La dernière publication depuis un thème spécifique
+@app.route('/search/last/<theme>')
+def latest_publication_by_theme(theme):
+    params = {
+        "search_query": f"all:{theme}",
+        "sortBy": "submittedDate",
+        "sortOrder": "descending",
+        "max_results": 5  # Si vous voulez afficher plus d'un résultat, modifiez cette valeur
+    }
+    response = requests.get(ARXIV_API_URL, params=params)
+    feed = feedparser.parse(response.content)
+    if not feed.entries:
+        return render_template('page.html', entries=[], message="Aucune publication récente trouvée pour le thème spécifié.")
+    
+    # Préparez les données pour toutes les entrées trouvées
+    entries = [{
+        'title': entry.get('title', 'Pas de titre'),
+        'authors': ', '.join(author.name for author in entry.authors),
+        'summary': entry.get('summary', 'Pas de résumé'),
+        'published': entry.get('published', 'Date de publication inconnue')
+    } for entry in feed.entries]
+    
     return render_template('page.html', entries=entries)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
