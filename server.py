2e538dd7e6b334496783379215bf0fac7bbf236e
kadzze <148210328+kadzze@users.noreply.github.com>
2024-04-02 22:49:17 +0200

    Route articles and article/number

--- server.py
+++ server.py
@@ -59,6 +59,53 @@ def get_title():
 
    return {'Title of the articles': title}
 
+
+#Root pour afficher toutes les infos des articles
+@app.route('/articles')
+def get_info():
+
+   with open('articles.json') as f:
+      data = json.load(f)
+
+   articles_info = []  # Pour stocker les informations des articles
+
+   for i, article in enumerate(data, start=1):
+        numero=i
+        category = article.get('category', 'N/A')
+        title = article.get('title', 'N/A')
+        url = article.get('url', 'N/A')
+        publication_date = article.get('date', 'N/A')
+        #article_info=f"Article number:{i} named {title} was published the {publication_date} and belongs to the category {category}. Its url is {url}"
+        article_info = {
+            'Numero of article':i,
+            'category': category,
+            'title': title,
+            'url': url,
+            'publication_date': publication_date
+        }
+        articles_info.append(article_info)
+   
+
+   return {'Informations of the articles': articles_info}
+
+def add_article_numbers(data):
+    for i, article in enumerate(data, start=1):
+        article['article_number'] = i
+    return data
+
+@app.route('/article/<int:number>')
+def get_article_description(number):
+    with open('articles.json') as f:
+        data = json.load(f)
+        data_with_numbers = add_article_numbers(data)
+    
+    if number > 0 and number <= len(data_with_numbers):
+        article = data_with_numbers[number - 1]
+        return {'Publication_Date':article['date'],'Title':article['title'],'Category':article['category'],'Description': article['description']}
+    else:
+        return {'error': 'Article number out of range'}, 404
+
+
 if __name__ == '__main__':
     app.run()
 
