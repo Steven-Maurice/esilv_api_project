e090bcea65a157764203d1a2ec3766cd314b2526
Yufei Li <yufeirainie@gmail.com>
2024-04-03 21:47:40 +0200

    Ajout de 1. _getdata à ArXiv_extractor.py

--- ArXiv_API/ArXiv_extractor.py
+++ ArXiv_API/ArXiv_extractor.py
@@ -1,44 +1,45 @@
 print("Hello world")
 
+# 1. _getdata
+
+# Rainie0000: Yufei
+from flask import Flask, jsonify
 import requests
-from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
 
-def fetch_article_details(entry):
-    article = {}
-    article['title'] = entry.find('title').text.strip() if entry.find('title') is not None else ""
-    article['published_date'] = entry.find('published').text if entry.find('published') is not None else ""
-    
-    authors = []
-    for author_tag in entry.find_all('author'):
-        authors.append(author_tag.find('name').text if author_tag.find('name') is not None else "")
-    article['authors'] = authors
-    
-    return article
+app = Flask(__name__)
 
-def fetch_articles(query, max_results=5):
-    url = f"http://export.arxiv.org/api/query?search_query={query}&max_results={max_results}"
+def retrieve_articles():
+    url = 'http://export.arxiv.org/api/query?search_query=cat:cs.CV&max_results=5'
     response = requests.get(url)
+    response_xml = ET.fromstring(response.content)
+
+    articles = []
+    for entry in response_xml.iter('{http://www.w3.org/2005/Atom}entry'):
+        title = entry.find('{http://www.w3.org/2005/Atom}title').text
+        author = entry.find('{http://www.w3.org/2005/Atom}author/{http://www.w3.org/2005/Atom}name').text
+        summary = entry.find('{http://www.w3.org/2005/Atom}summary').text
+
+        article = {
+            'title': title,
+            'author': author,
+            'summary': summary
+        }
+        articles.append(article)
+
+    return articles
+
+@app.route('/get_data')
+def get_data():
+    articles = retrieve_articles()
+
+    # Récupérez les 5 premiers articles
+    five_articles = articles[:5]
+
+    # Retournez les articles en tant que réponse JSON
+    return jsonify(five_articles)
+
+if __name__ == '__main__':
+    app.run()
     
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        entries = soup.find_all('entry')
-        
-        articles = []
-        for entry in entries:
-            article = fetch_article_details(entry)
-            articles.append(article)
-        
-        return articles
-    else:
-        print("Erreur lors de la récupération des articles:", response.status_code)
-        return []
-
-query = "all:electron"
-max_results = 5
-articles = fetch_articles(query, max_results)
-
-for article in articles:
-    print("Titre:", article['title'])
-    print("Date de publication:", article['published_date'])
-    print("Auteurs:", ", ".join(article['authors']))
-    print()
+    
