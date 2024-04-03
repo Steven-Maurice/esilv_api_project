d36fd8e99d2f87529a7ee40c8d9dea7d0e270b20
paulbourcereau <paul.bourcereau@icloud.com>
2024-04-03 15:32:14 +0200

    First few files

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from huggingfacepapers import fetch
 
 app = Flask(__name__)
+cached_articles = []
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return 'Hello, World!' 
+
+@app.route('/get_data')
+def get_data():
+    global cached_articles
+    cached_articles = fetch.articles()
+    return cached_articles
+    return {"number_of_articles":len(cached_articles)}  
+
+@app.route("/articles")
+def articles():
+    global cached_articles 
+    return cached_articles
 
 if __name__ == '__main__':
     app.run()
