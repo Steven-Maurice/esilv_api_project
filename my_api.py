20eddde80f84fb7c2ccf8aa5cc7909f9ec6c30ca
sama <dadaru>
2024-04-04 13:13:58 +0200

    squelette code création API

--- my_api.py
+++ my_api.py
@@ -12,6 +12,19 @@ app = Flask(__name__)
 def home():
     return "Home"
 
+@app.route("/get_data")
+def get_data():
+    return "Fetching data"
+
+@app.route("/articles")
+def get_articles():
+    return "Infos about articles"
+
+
+@app.route("/articles/<number>")
+def get_content(number):
+    return "Content of an article"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
