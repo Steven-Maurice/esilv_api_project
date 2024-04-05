effb436dbdbdb6854f97dfabbe041a0e8eb51829
Alexandre Deroux <alexandre.deroux@gmail.com>
2024-04-05 10:59:50 +0200

    Update main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from function_get_data import get_data
+from function_articles import articles
 
 app = Flask(__name__)
 
 app.register_blueprint(get_data)
+app.register_blueprint(articles)
 
 if __name__ == '__main__':
     app.run(debug=True)
