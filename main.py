a96ca32ee936f64fdfffd366aa5c03fceb505dc9
Alexandre Deroux <alexandre.deroux@gmail.com>
2024-04-05 15:58:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from function_get_data import get_data
-from function_articles import articles
+from function_articles import articles,article
 from function_ml import ml_positive, ml_negative
 
 app = Flask(__name__)
 
 app.register_blueprint(get_data)
 app.register_blueprint(articles)
+app.register_blueprint(article)
 
 if __name__ == '__main__':
     app.run(debug=True)
