cdc89022ed33b3fbb046d3bd13513fc88cd88afe
Yufei Li <yufeirainie@gmail.com>
2024-04-06 13:45:27 +0200

    Update 4._ml à ArXiv_extractor.py

--- ArXiv_API/ArXiv_extractor.py
+++ ArXiv_API/ArXiv_extractor.py
@@ -1,18 +1,18 @@
 
-# _getdata
+
+from os import abort
+from flask import Flask, jsonify, redirect
+import requests 
+import xml.etree.ElementTree as ET
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Rainie0000: Yufei Li
 
-
-from os import abort
-from flask import Flask, jsonify, redirect # type: ignore
-import requests # type: ignore
-import xml.etree.ElementTree as ET
-
-
+# _getdata
 
 # création de l'instance de Flask
-
 app = Flask(__name__)
 
 # Je récupère les articles à partir de l'API ArXiv
@@ -64,6 +64,7 @@ def get_data():
 
 
 # valentinf75 : Valentin Fried
+
 @app.route('/articles')
 def articles():
     articles = retrieve_articles()
@@ -93,6 +94,92 @@ def article(number):
         return redirect(article_link)  # Rediriger l'utilisateur vers le lien de l'article
     else:
         abort(404)  # Si l'article n'existe pas, renvoie une erreur 404
+
+
+
+
+# Rainie0000 : Yufei Li
+
+# 4. /ml 
+
+# Ici j'utilise des elements de machine learning 
+# comme TfidfVectorizer,Cosine Similarity, perform_recommendation()
+
+# Idée est que j'extrais 100 articles 
+# je donne un mot clé et je veux qu'on me 
+# recommende l'article (parmi les 100) qui contient plus probablement les éléments 
+# qui m'intéressent
+
+def retrieve_4_articles():
+    url = 'http://export.arxiv.org/api/query?search_query=cat:cs.CV+AND+ti:"image+processing"&max_results=100'
+    response = requests.get(url)
+    response_xml = ET.fromstring(response.content)
+
+    articles_4 = []
+    for entry in response_xml.findall('{http://www.w3.org/2005/Atom}entry'):
+        article = {}
+
+        # Extraction des informations de chaque article
+        title = entry.find('{http://www.w3.org/2005/Atom}title').text
+        summary = entry.find('{http://www.w3.org/2005/Atom}summary').text
+        link = entry.find('{http://www.w3.org/2005/Atom}link').attrib['href']
+
+        article['title'] = title
+        article['summary'] = summary
+        article['link'] = link
+
+        articles_4.append(article)
+
+    return articles_4
+
+# Je récupère mes articles 
+articles_4 = retrieve_4_articles()
+
+# Je crée une représentation vectorielle des articles
+corpus = [article['summary'] for article in articles_4]
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(corpus)
+
+# Création d'endpoint pour recommander un article
+@app.route('/ml/recommendation')
+def recommend_article():
+    
+    # J'ai choisi comme mot clé "image processing"
+    # Après on peut remplacer "image processing" par d'autres mots clés
+    recommended_article_index = perform_recommendation("image processing")
+
+    return redirect_article(recommended_article_index)
+
+# J'effectue la recommandation d'article
+def perform_recommendation(content):
+    # Je transforme le contenu spécifié en représentation vectorielle
+    content_vector = vectorizer.transform([content])
+
+    # Je calcule les similarités cosinus entre le contenu spécifié et les articles
+    similarities = cosine_similarity(content_vector, X).flatten()
+
+    # Je veux l'indice de l'article le plus similaire
+    recommended_article_index = similarities.argmax()
+
+    return recommended_article_index
+
+# Endpoint pour rediriger l'utilisateur vers le lien de l'article
+@app.route('/articles/<int:number>')
+def redirect_article(number):
+    articles = retrieve_4_articles()
+    if 1 <= number <= len(articles):
+        article_link = articles[number-1]['link']
+        return redirect(article_link)  # Rediriger l'utilisateur vers le lien de l'article
+    else:
+        abort(404)  # Si l'article n'existe pas, renvoie une erreur 404
+    
+        
+# Conclusion: lorsque j'exécute le code, j'ai un lien qui me dirige directement vers l'article
+# recommendé parmi les 100 articles
+
+# Par exemple avec le mot clé "Image Processing”,
+# l'article recommandé est "Convolutional Neural Pyramid for Image Processing"
+
 
 if __name__ == '__main__':
     app.run()
