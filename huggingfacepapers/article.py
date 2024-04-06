677210be1a6f0e76edb2949e14458f6138ec6951
cystalmae <ilfaitbeau95@gmail.com>
2024-04-06 10:26:35 +0200

    functions for class Article

--- huggingfacepapers/article.py
+++ huggingfacepapers/article.py
@@ -1,4 +1,5 @@
 import requests 
+from bs4 import BeautifulSoup
 
 class Article:
     def __init__(self, url, title=None, img=None, upvotes=None, authors=None):
@@ -8,3 +9,35 @@ class Article:
         self.authors = authors
         self.url = url
         self.abstract = None
+
+
+    def load(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        self.authors = [
+            author.text.strip() for author in soup.select("span.author button")
+        ]
+        self.abstract = soup.select_one("h2 + p").text
+        self.pdfUrl = soup.select_one("div a + a").attrs["href"]
+
+    def preview(self):
+        return {
+            "id": self.id,
+            "img": self.img,
+            "title": self.title,
+            "n_authors": self.n_authors,
+            "url": self.url,
+        }
+
+    def detail(self):
+        return {
+            "id": self.id,
+            "img": self.img,
+            "title": self.title,
+            "n_authors": self.n_authors,
+            "url": self.url,
+            "authors": self.authors,
+            "abstract": self.abstract,
+            "pdfUrl": self.pdfUrl,
+            "embedding": self.embedding.tolist(),
+        }
