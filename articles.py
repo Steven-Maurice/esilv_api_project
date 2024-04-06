ac75ac947084baca0e2fa3c61fdac5d1c5ceb1ea
cirier <philippe.cirier@hotmail.com>
2024-04-06 19:54:58 +0200

    articles

--- articles.py
+++ articles.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
-
-
+ 
 app = Flask(__name__)
-
+ 
 def scrape_articles():
     url = "https://www.artificialintelligence-news.com/"
     page = requests.get(url)
@@ -15,9 +14,15 @@ def scrape_articles():
         link = article.find("a")["href"]
         articles.append({"title": title, "url": link})
     return articles
-
-@app.route('/get_data', methods=['GET'])
-def get_data():
+ 
+ 
+@app.route('/articles', methods=['GET'])
+def articles():
     articles = scrape_articles()
-    return jsonify(articles[:5])  
+    return jsonify(articles[:5])
+ 
+
+@app.route('/article/<number>', methods=['GET'])
+def article(number):
+    return jsonify({"error": "Endpoint not implemented"})
  
