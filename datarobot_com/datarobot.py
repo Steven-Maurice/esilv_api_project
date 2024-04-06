59c792027ba5e0f42324174b1df0eb13b0ed19d0
PierreCAVALLI27 <pierrecavalli24@gmail.com>
2024-04-06 16:13:17 +0200

    add /articles to get title and the date

--- datarobot_com/datarobot.py
+++ datarobot_com/datarobot.py
@@ -1,7 +1,9 @@
 from flask import Flask, Response
-
+import os
 from base.request import Request
 from base.scrap_content import return_5_most_recent
+import codecs
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -27,8 +29,28 @@ def get_data():
 
 
 @app.route("/articles")
-def articles(articles):
-    return articles
+def articles():
+    dossier_html = "/Users/pierrecavalli/Projects/A4/python/esilv_api_project/datarobot_com/windows"
+    articles_data = []
+
+    for filename in os.listdir(dossier_html):
+        if filename.endswith(".html"):
+            filepath = os.path.join(dossier_html, filename)
+            with codecs.open(filepath, "r", "utf-8") as file:
+                soup = BeautifulSoup(file.read(), "html.parser")
+                title = soup.title.string if soup.title else "Titre non trouvé"
+
+                meta_tag = soup.find("meta", property="article:modified_time")
+                date = meta_tag["content"] if meta_tag else "Date non trouvée"
+                articles_data.append({"title": title, "date": date})
+
+    html_response = "<html><head><title>Liste des Articles</title></head><body>"
+    html_response += "<h1>Articles</h1><ul>"
+    for article in articles_data:
+        html_response += f"<li><strong>Titre :</strong> {article['title']} - <strong>Date :</strong> {article['date']}</li>"
+    html_response += "</ul></body></html>"
+
+    return Response(html_response, mimetype="text/html")
 
 
 @app.route("/article/<int:a_number>")
