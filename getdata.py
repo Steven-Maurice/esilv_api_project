cef508d02fc4178f8257e29e0c8ca639abf6de60
cirier <philippe.cirier@hotmail.com>
2024-04-06 19:54:39 +0200

    get_data

--- getdata.py
+++ getdata.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
- 
+
+
 app = Flask(__name__)
- 
+
 def scrape_articles():
     url = "https://www.artificialintelligence-news.com/"
     page = requests.get(url)
@@ -15,7 +16,8 @@ def scrape_articles():
         articles.append({"title": title, "url": link})
     return articles
 
-@app.route('/ml', defaults={'number': None}, methods=['GET'])
-@app.route('/ml/<number>', methods=['GET'])
-def ml(number):
-    return jsonify({"error": "ML Endpoint not implemented"})
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    articles = scrape_articles()
+    return jsonify(articles[:5])  # Limit to 5 articles
+ 
