1db2210d002c3f1de156638b12bbfbdc465837fe
cirier <philippe.cirier@hotmail.com>
2024-04-07 15:14:32 +0200

    fixed main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ml import ml_blueprint
 
 app = Flask(__name__)
 
-app.register_blueprint(getdata)
+app.register_blueprint(get_data)
 app.register_blueprint(articles_blueprint, url_prefix='/articles') 
 app.register_blueprint(ml_blueprint, url_prefix='/ml')  
 
