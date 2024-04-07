dad81057b81ab63bebff43503fde9bfa55c9d7fa
BaptisteBottosesilv <bbottos79@gmail.com>
2024-04-07 12:44:37 +0200

    First commit

---
+++ scraping.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+import json
+from selenium.webdriver.common.by import By
+
+def scrape():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://blog.google/technology/ai/")
+    print('-----------------------------------------------------')
+    print(f"Connected to the website: {driver.title}")
+    print('-----------------------------------------------------')
+
+    articles = driver.find_elements(By.CSS_SELECTOR, "div a.feed-article__overlay")
+
+
+
+    data = []
+
+    for article in articles:
+
+        href = article.get_attribute("href") 
+        text = article.text.split("\n")
+
+        article_data = {
+            "url": href,
+            "category":text[0],
+            "title": text[1],
+            "description": text[2],
+            "date": text[3]
+        }
+        data.append(article_data)
+
+    with open('articles.json', 'w') as outfile:
+        json.dump(data, outfile)
+        
+    driver.quit()
+
+    print('-----------------------------------------------------')
+    print(f"The website is scrapped and the info are written in 'articles.json'")
+
+    print('-----------------------------------------------------')
+
+
+
--- server.py
+++ server.py
@@ -1,10 +1,36 @@
 from flask import Flask
+import json
+from collections import Counter
+from scraping import scrape
+from flask import jsonify
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+
+@app.route('/get_data')
+def get_data():
+    scrape()
+    return 'Scrapping is complete'
+
+@app.route('/nb_articles')  
+def get_nb_articles():
+    
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+    
+    return {'Number of articles': len(data)}
+
+@app.route('/scraped_data')
+def get_scraped_data():
+    with open('articles.json') as json_file:
+        data = json.load(json_file)
+    
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run()
