b67873fee636f7a9bf5cbfc6db44c9097af64c3e
Rémi DAI <remi.dai@edu.devinci.fr>
2024-04-07 13:25:44 +0200

    Codes with more endpoints and fix some bugs

--- arxiv/functions.py
+++ arxiv/functions.py
@@ -4,15 +4,17 @@ Created on Sun Apr  7 01:56:47 2024
 
 @author: remid
 """
+#!pip install vaderSentiment
 
 import requests
 from xml.etree import ElementTree #pour lire les données de l'api arxiv car je n'arrive pas à convertir les données en json
 import datetime
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 url = 'https://export.arxiv.org/api/query'
 
 #stock the data in a 
-def arxiv_data(search_query,nb_results=5):
+def arxiv_data(search_query,nb_results=8):
   params = {
       'search_query': search_query,
       'max_results': nb_results
@@ -29,8 +31,7 @@ def arxiv_data(search_query,nb_results=5):
 #function that take the information from each article
 def get_articles():
   articles = []
-  query = input("Give me a key word of what articles you are searching : ")
-  data = arxiv_data(f"all:{query}") #fetch 5 articles from the specified category
+  data = arxiv_data("all:AI") #fetch 5 articles from the specified category
   path = '{http://www.w3.org/2005/Atom}'
   for entry in data.findall(path+'entry'): #feed contains child <entry> elements with each <entry> representing an article
     title = entry.find(path+'title').text
@@ -48,3 +49,13 @@ def get_articles():
   articles.sort(key=lambda x: x[1], reverse = True)
   return articles
 
+#function that classify a sentence into positive/negative/neutral
+def ml_sentiments(text):
+  analyzer = SentimentIntensityAnalyzer()
+  sentiment_dict = analyzer.polarity_scores(text)
+  if sentiment_dict['compound']>= 0.05:
+    return 'positive'
+  elif sentiment_dict['compound'] <= -0.05 :
+    return 'negative'
+  else:
+    return 'neutral'
